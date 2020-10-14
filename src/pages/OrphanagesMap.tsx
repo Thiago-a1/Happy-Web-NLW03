075112bf9d771fdd70e4d7506ad2c9504b7e92c0
Thiago Araujo <thiago.araujo.r11@gmail.com>
2020-10-14 08:25:33 -0300

    first commit

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
+
+function OrphanagesMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Rio de janeiro</strong>
+          <span>Rio de janeiro</span>
+        </footer>
+      </aside>
+
+      <Map 
+        center={[-22.9163403,-43.2299227]}
+        zoom={17}
+        style={{ width: '100%', height: '100%'}}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
+}
+
+export default OrphanagesMap;
